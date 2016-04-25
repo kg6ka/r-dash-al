eef0c98d7c6d06e2e1ac34895c3e911d4a03a176
Nusinka <anushkanusik@gmail.com>
2016-04-25 16:17:35 +0300

    create fleet activity component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export Anomalies from './Anomalies/Anomalies';
 export CarsStatus from './CarsStatus/CarsStatus';
 export ViewPercent from './ViewPercent/ViewPercent';
+export FleetActivity from './FleetActivity/FleetActivity';

99e7e62eb2b021b39bc2a22a8168a060fbe8130e
Aron <astein@linnovate.net>
2016-05-04 16:01:35 +0300

    add img and fonts

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -23,33 +23,46 @@ export default class Dashboard extends Component {
     return (
       <div>
         <HeaderSite/>
-        <div className={cx(styles.backgroundGradient, styles.fleetStatus)}>
-          <CarsStatus
-            registeredCars={'35,203'}
-            percentRegistered={12}
-            active={Math.random() * 100}
-            uptodate={Math.random() * 100}
-            updateData={'13.01.16'}
-          />
-          <Anomalies
-            anomalies={310}
-            cars1={90}
-            percent={10}
-            percentRight={Math.random() * 100}
-            unblocked={100}
-            cars2={89}
-            blocked={210}
-            cars3={5}
-          />
-        </div>
-        <div className={styles.fleetStatus}>
-          <div className={cx(styles.backgroundGradient, styles.fleetActivity)}>
-            <FleetActivity />
+        <div className={styles.layout}>
+          <div className={cx(styles.layoutSideRight,styles.layoutCol20)}>
+             <img src="/assets/images/3.jpg"/>
           </div>
-          <div className={styles.fleetActivity}>
-            <Categories />
+          <div className={cx(styles.layoutSideLeft,styles.layoutCol80)}>
+            <div className={cx(styles.backgroundGradient, styles.fleetStatus)}>
+              <CarsStatus
+                registeredCars={'35,203'}
+                percentRegistered={12}
+                active={Math.random() * 100}
+                uptodate={Math.random() * 100}
+                updateData={'13.01.16'}
+              />
+              <Anomalies
+                anomalies={310}
+                cars1={90}
+                percent={10}
+                percentRight={Math.random() * 100}
+                unblocked={100}
+                cars2={89}
+                blocked={210}
+                cars3={5}
+              />
+            </div>
+            <div className={cx(styles.layoutSideRight,styles.layoutCol25)}>
+              <Target />
+              <img src="/assets/images/2.jpg"/>
+            </div>
+            <div className={styles.fleetStatus,styles.layoutSideLeft,styles.layoutCol75}>
+              <div className={cx(styles.backgroundGradient, styles.fleetActivity)}>
+                <FleetActivity />
+              </div>
+              <div className={cx(styles.fleetActivity,styles.layoutCol50)}>
+                <Categories />
+              </div>
+              <div className={styles.layoutCol50}>
+                 <img src="/assets/images/1.jpg"/>
+              </div>
+            </div>
           </div>
-          <Target />
         </div>
       </div>
     );

09b20dfb9f87e82c1484dc40fc4cfb317966978b
Your Name <you@example.com>
2016-05-10 14:11:33 +0300

    map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,54 +1,26 @@
-var React = require('react');
-import styles from './Map.scss';
-var Map = React.createClass({
+import { Map } from './components'
+import { connect } from 'react-redux'
 
-    componentDidMount(){
 
-        // Only componentDidMount is called when the component is first added to
-        // the page. This is why we are calling the following method manually.
-        // This makes sure that our map initialization code is run the first time.
-
-        this.componentDidUpdate();
-    },
-
-    componentDidUpdate(){
-
-        if(this.lastLat == this.props.lat && this.lastLng == this.props.lng){
-
-            // The map has already been initialized at this address.
-            // Return from this method so that we don't reinitialize it
-            // (and cause it to flicker).
-
-            return;
-        }
-
-        this.lastLat = this.props.lat;
-        this.lastLng = this.props.lng
-
-        var map = new GMaps({
-            el: '#map',
-            lat: this.props.lat,
-            lng: this.props.lng
-        });
-
-        // Adding a marker to the location we are showing
-
-        map.addMarker({
-            lat: this.props.lat,
-            lng: this.props.lng
-        });
-    },
-
-    render(){
-
-        return (
-            <div className={styles.mapHolder}>
-                <p>Loading...</p>
-                <div className={styles.map} id="map"></div>
-            </div>
-        );
+const mapStateToProps = (state) => {
+    return {
+        locations: state.map.locations,
     }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchLocations: () => {
+            dispatch({type: 'FETCH_LOCATIONS'})
+        }
+    }
+}
 
-});
 
-module.exports = Map;
+
+const MapContainer = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Map)
+
+export default MapContainer
+

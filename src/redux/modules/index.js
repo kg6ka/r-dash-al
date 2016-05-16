ade9b028a25c5bba757bb7d3a95114e339474031
Alex Vasylenko <o.vasylenko@wookieelabs.com>
2016-05-16 10:56:37 +0300

    Include new reducers for new modules

--- src/redux/modules/index.js
+++ src/redux/modules/index.js
@@ -1,17 +1,30 @@
 import { combineReducers } from 'redux';
 import currentUser, { sagas as currentUserSagas } from './currentUser';
 import alerts from './alerts';
-import map from './map';
-import categories from './categories';
-
+import carsStatus, { sagas as carsStatusSagas } from './carsStatus.js';
+import totalAnomalies, { sagas as totalAnomaliesSagas } from './totalAnomalies.js';
+import fleetActivities, { sagas as fleetActivitiesSagas } from './fleetActivities.js';
+import categories, { sagas as categoriesSagas } from './categories.js';
+import target, { sagas as targetSagas } from './target.js';
+import map, { sagas as mapSagas } from './map.js';
 
 export default combineReducers({
   currentUser,
   alerts,
   map,
-  categories
+  categories,
+  carsStatus,
+  totalAnomalies,
+  fleetActivities,
+  target,
 });
 
 export const sagas = [
   ...currentUserSagas,
+  ...carsStatusSagas,
+  ...totalAnomaliesSagas,
+  ...fleetActivitiesSagas,
+  ...categoriesSagas,
+  ...targetSagas,
+  ...mapSagas,
 ];

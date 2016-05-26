0a6c03290daf5ca7f775c99018630ddd6155ac1e
Alex Vasylenko <o.vasylenko@wookieelabs.com>
2016-05-26 20:59:47 +0300

    Pause works when we change page

--- src/containers/AnomaliesPage/AnomaliesPage.js
+++ src/containers/AnomaliesPage/AnomaliesPage.js
@@ -54,10 +54,6 @@ export default class AnomaliesPage extends Component {
     }
   }
 
-  componentWillUnmount() {
-    this.props.removeTime();
-  }
-
   componentWillReceiveProps(props) {
     if (props.getTags.currentTag !== this.props.getTags.currentTag && props.getTags.currentTag) {
       this.getNewProps(props.getTags.currentTag);

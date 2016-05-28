13d943715f575b3c2b48542dda8a85e731104cd1
Alex Vasylenko <o.vasylenko@wookieelabs.com>
2016-05-28 15:08:44 +0300

    Fix bug with position for blocks into show all signals list

--- src/components/AnomaliesList/AnomaliesList.js
+++ src/components/AnomaliesList/AnomaliesList.js
@@ -273,7 +273,7 @@ export default class AnomaliesList extends Component {
 
   signalValuesRender(item) {
     return (<div className={styles.singleSignal}>
-      <div>
+      <div className={styles.signalBlock}>
         <img
           className={styles.signalIcon}
           src={ signal }
@@ -281,7 +281,7 @@ export default class AnomaliesList extends Component {
           />
         CamZoomActiveState = {item.name}
       </div>
-      <div>
+      <div className={styles.signalBlock}>
         <img
           className={styles.prevIcon}
           src={ previous }

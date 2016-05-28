133c62b2f989440946737d2133eb73860f085786
Nusinka <anushkanusik@gmail.com>
2016-05-28 10:50:32 +0300

    Add export button

--- src/components/BackBtn/BackBtn.js
+++ src/components/BackBtn/BackBtn.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router';
 import styles from './BackBtn.scss';
 
-export default () => (
-  <div className={styles.backBtn}>
-    <Link to="/dashboard" >Dashboard</Link>
-  </div>
-);
+export default class AnomaliesList extends Component {
+  handler() {
+    console.log('Export table');
+  }
+
+  render() {
+    return (
+      <div className={styles.header}>
+        <div className={styles.backBtn}>
+          <Link to="/dashboard" >Dashboard</Link>
+        </div>
+        <div className={styles.export} onClick={this.handler.bind(this)}>
+          EXPORT
+        </div>
+      </div>
+    );
+  }
+}
